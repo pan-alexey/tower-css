26736d3c668a014e08daca622313067454481178
alexey <pan-alexey@live.com>
2019-09-29 15:00:41 +0300

    ripple fix

---
+++ app/webpack/components/Dropdown.vue
@@ -0,0 +1,57 @@
+<style lang="less">
+.box-example{
+    display: inline-block;
+    width: 12rem;
+    height: 5rem;
+    margin: .25rem;
+}
+</style>
+
+
+
+<template>
+  <div class="activity">
+    <div class="navbar">
+      <div class="navbar-top">
+        <div class="navbar-block blue" data-type="float">
+          <div class="navbar-content">
+            <div class="navbar-group blue">
+              <router-link to="/">
+                <i class="material-icons icon-home">&#xe88a;</i>
+              </router-link>
+            </div>
+            <div class="navbar-title center">
+              <b>BOX</b>
+            </div>
+            <div class="navbar-group">
+              <router-link to="/components">
+                <i class="material-icons icon-list">&#xe896;</i>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container gutter-x">
+
+
+        <span class="box-material box-example drop">
+
+        </span>
+
+
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted: function() {
+    //Необходимо вызвать пересчет отступа
+    window.navbar();
+  }
+};
+</script>
+
